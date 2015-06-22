463a792069792cf509aa931eacc2e1c5f41312db
Daniel Gorelick <dqgorelick@gmail.com>
2015-06-22 18:06:42 -0400

    added image functionality

--- canvas demo/js/app.js
+++ canvas demo/js/app.js
@@ -7,21 +7,57 @@ Functions to decide slides
 
 (function(){
 	var tests = {
-		"articleTop": false
+		"articleTop": false,
+		"fontTest": false,
+		"styles": true
 	}
 
-	// from article: << http://www.nytimes.com/2015/06/22/us/california-golf-trip-lands-obama-in-a-water-use-debate.html?ref=us&_r=0 >>
-	var headline = "California Golf Trip Lands \nObama in a Water-Use Debate";
-	var photo = "http://static01.nyt.com/images/2015/06/22/us/21OBAMA/21OBAMA-master675-v3.jpg";
-	var photo_tagline = "President Obama at the airport in Palm Springs, Calif., on Saturday, before going to the Sunnylands estate in Rancho Mirage. Credit Zach Gibson/The New York Times"
-	var date = "JUNE 21, 2015"
-	var author = "By GARDINER HARRIS"
-	var section = "POLITICS"
-	var kicker = "WHITE HOUSE MEMO"
+	results = {
+		article1: {
+			"source": "http://www.nytimes.com/2015/06/22/us/california-golf-trip-lands-obama-in-a-water-use-debate.html?ref=us&_r=0",
+			"headline": "California Golf Trip Lands \nObama in a Water-Use Debate",
+			"photo": "http://static01.nyt.com/images/2015/06/22/us/21OBAMA/21OBAMA-master675-v3.jpg",
+			"photo_tagline": "President Obama at the airport in Palm Springs, Calif., on Saturday, before going to the Sunnylands estate in Rancho Mirage. Credit Zach Gibson/The New York Times",
+			"date": "JUNE 21, 2015",
+			"author": "By GARDINER HARRIS",
+			"section": "POLITICS",
+			"kicker": "WHITE HOUSE MEMO"
+		},
+		article2: {
+			// from article: << http://local.sbx.nytimes.com/2015/06/22/fashion/mens-style/in-milan-the-mens-fashion-crowd-practices-magazine-diplomacy.html >>
+			"headline": "In Milan, the Men’s Fashion Crowd Practices Magazine Diplomacy",
+			"photo":[
+				"http://static01.nyt.com/images/2015/06/22/fashion/Scene-CITY-gq-slide-BJB3/Scene-CITY-gq-slide-BJB3-articleLarge.jpg",
+				"http://static01.nyt.com/images/2015/06/22/fashion/Scene-CITY-gq-slide-D4NP/Scene-CITY-gq-slide-D4NP-articleLarge.jpg",
+				"http://static01.nyt.com/images/2015/06/22/fashion/Scene-CITY-gq-slide-82GI/Scene-CITY-gq-slide-82GI-articleLarge.jpg",
+				"http://static01.nyt.com/images/2015/06/22/fashion/Scene-CITY-gq-slide-3N9C/Scene-CITY-gq-slide-3N9C-articleLarge.jpg",
+				"http://static01.nyt.com/images/2015/06/22/fashion/Scene-CITY-gq-slide-0Q6E/Scene-CITY-gq-slide-0Q6E-articleLarge.jpg",
+				"http://static01.nyt.com/images/2015/06/22/fashion/Scene-CITY-gq-slide-TKR8/Scene-CITY-gq-slide-TKR8-articleLarge.jpg"
+			],
+			"photo_tagline": [
+				"Miguel performing during the GQ party for Jim Moore.",
+				"Jim Nelson, right, and Jim Moore at the GQ party.",
+				"Neil Barrett and Thom Browne at the GQ party.",
+				"Dan Peres, right, and Tomas Maier, center at the Details party.",
+				"The Details magazine party.",
+				"Dan Peres, Constanza Etro and Kean Etro at the Details party."
+			],
+			"photographer": [
+				"JACOPO RAULE / GETTY IMAGES FOR GQ",
+				"JACOPO RAULE / GETTY IMAGES FOR GQ",
+				"JACOPO RAULE / GETTY IMAGES FOR GQ",
+				"VITTORIO ZUNINO CELOTTO / GETTY IMAGES FOR DETAILS MAGAZINE",
+				"VITTORIO ZUNINO CELOTTO / GETTY IMAGES FOR DETAILS MAGAZINE",
+				"VITTORIO ZUNINO CELOTTO / GETTY IMAGES FOR DETAILS MAGAZINE"
+			],
+			"date": "JUNE 21, 2015",
+			"author": "By GARDINER HARRIS",
+			"section": "POLITICS",
+			"kicker": "WHITE HOUSE MEMO"
+		}
+	}
 
-	//create black background
-	var path = new Path.Rectangle(new Point(0,0), new Size(view.size))
-	path.fillColor = "#000000";
+	var path = new Path.Rectangle(new Point(0,0), new Size(view.size)).fillColor = "#000000";
 
 	if(tests.articleTop){
 		//	winky face
@@ -61,24 +97,45 @@ Functions to decide slides
 		});
 	}
 
-	var nyt = new PointText({
-		point: new Point(view.size._width/2,100),
-		justification: 'center',
-		fontSize: 35,
-		fillColor: "#FFFFFF",
-		content: "NYTKarnakDisplay",
-		fontFamily: "NYTKarnakDisplay"
-	})
+	if(tests.fontTest){
+		var nyt = new PointText({
+			point: new Point(view.size._width/2,100),
+			justification: 'center',
+			fontSize: 35,
+			fillColor: "#FFFFFF",
+			content: "NYTKarnakDisplay",
+			fontFamily: "NYTKarnakDisplay"
+		})
 
-	var times = new PointText({
-		point: new Point(view.size._width/2,200),
-		justification: 'center',
-		fontSize: 35,
-		fillColor: "#FFFFFF",
-		content: "NYTCheltenhamLt",
-		fontFamily: "NYTCheltenhamLt"
-	})
+		var times = new PointText({
+			point: new Point(view.size._width/2,200),
+			justification: 'center',
+			fontSize: 35,
+			fillColor: "#FFFFFF",
+			content: "NYTCheltenhamLt",
+			fontFamily: "NYTCheltenhamLt"
+		})
+	}
 
+	if(tests.styles){
+		var photos = [];
+		results.article2.photo.forEach(function(photo, it){
+			console.log(it + " " + photo);
+			photo[it] = imageSize(new Raster('results.article2.photo['+it+']'));
+		});
+	}
+
+	function imageSize(raster){
+		if(raster.width > raster.height){
+			var ratio = (view.size._width)/raster.width;
+			raster.position = new Point(raster.width*ratio/2, raster.height*ratio/2);
+			raster.scale(ratio);
+		} else {
+			var ratio = (view.size._height)/raster.height;
+			raster.position = new Point(view.size._width/2, raster.height*ratio/2);
+			raster.scale(ratio);
+		}
+	}
 
 	//	clicking interface
 	var mouse = false;
@@ -86,7 +143,6 @@ Functions to decide slides
 	function onMouseDown(event){
 		mouse = true;
 	}
-
 	function onMouseUp(event){
 		mouse = false;
 		count = 0;
