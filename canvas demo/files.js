fe7ad5e986b9160a00aa0aa35e0c2843e79b627d
Daniel Gorelick <dqgorelick@gmail.com>
2015-06-22 14:42:07 -0400

    commit to add font files

---
+++ canvas demo/files.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var css = "";
+css += "-------------FONTS-------------\n"
+
+fs.readdirSync('./fonts').forEach(function(name){
+	css += "@font-face { \n\tfont-family: '" + name +"';\n\tsrc: url('./fonts/"+name+"')\n}\n"
+})
+
+css += "-------------CSS-------------\n"
+fs.readdirSync('./fonts').forEach(function(name, it){
+	css += "#loadfonts p:nth-child("+(it+1)+"){font-family: '"+name+"';}\n";
+})
+
+css += "-------------HTML-------------\n"
+fs.readdirSync('./fonts').forEach(function(name){
+	css += "<p>"+name+"</p>\n"
+})
+
+fs.writeFile('files.txt', css, function(err){
+	if (!err) {
+		console.log("saved!");
+	}
+})
