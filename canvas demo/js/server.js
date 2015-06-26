62ab431a4b416be1890b4bf87c2908147e4f43d0
Sam Kortchmar <samuel.kortchmar@nytimes.com>
2015-06-26 15:59:43 -0400

    ffmpeg in server

--- canvas demo/js/server.js
+++ canvas demo/js/server.js
@@ -3,6 +3,8 @@ var app 			= express();
 var bodyParser 		= require('body-parser');
 var port 			= process.env.PORT || 8080;
 var router 			= express.Router();
+var ffmpeg 			= require('fluent-ffmpeg');
+
 
 //middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,11 +21,27 @@ router.route('/data')
 		raw = req.body.name;
 		res.json({ message: 'received'})
 		console.log("raw ", raw);
-	})
+	});
 
 	.get(function(req, res){
 		raw = raw || 'none'
 		res.json({ message: raw })
+	});
+
+router.route('/done')
+	.get(function(req, res) {
+		var proc = ffmpeg('tmp/%d.png')
+		  .videoCodec('libx264')
+		  .outputOptions(['-pix_fmt yuv420p'])
+		  // setup event handlers
+		  .on('end', function() {
+			      req.json({message : 'file has been converted succesfully'});
+		  })
+		  .on('error', function(err) {
+			      req.json(message : 'failed conversion.');
+		  })
+		  // save to file
+		  .save('output.mp4');
 	});
 
 // http://localhost:8080/api
@@ -34,19 +52,3 @@ router.get('/',function(req, res) {
 app.use('/api', router);
 app.listen(port);
 console.log('hosting on port ' + port);
-
-/*
-
-app.post('/api/data', function(req, res){
-	var user_name=req.body.user;
-	var password=req.body.password;
-	console.log("User name = "+user_name+", password is "+password);
-	res.end("yes");
-});
-
-$.post("http://localhost:3000/login",{user: user,password: pass}, function(data){
-	if(data==='done'){
-		alert("login  success");
-	}
-});
- */
