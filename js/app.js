c198fce34a8a5f822299f57f91fb4793e0309b13
Sam Kortchmar <samuel.kortchmar@nytimes.com>
2015-07-17 15:20:48 -0400

    timeline scrolly

--- js/app.js
+++ js/app.js
@@ -7,3 +7,15 @@ clear()
 */
 
 var app = angular.module ('timesTrailer', ['Assets', 'Editor', 'Canvas', 'Timeline', 'ConfigService', 'AssetService']);
+app.directive("scroll", function ($window) {
+    return function($scope, element, attrs) {
+        var eltWidth = 200;
+        var index = 0;
+        angular.element(element).bind("scroll", function(event) {
+            var newIndex = Math.round(this.scrollLeft / eltWidth);
+            if (newIndex != index) {
+                index = newIndex;
+            }
+        });
+    };
+});
--- tools/files.js
+++ tools/files.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var css = "";
 // css += "-------------FONTS-------------\n"
 
-fs.readdirSync('../assets/fonts').forEach(function(name){
+fs.readdirSync('../fonts').forEach(function(name){
 	// css += "@font-face { \n\tfont-family: '" + name +"';\n\tsrc: url('./fonts/"+name+"')\n}\n"
 	css += "<div class='font-family' onclick='selectFont(this)' style='font-family:"+name+"' value='"+name+"'>"+name+"</div>\n";
 })
@@ -13,7 +13,7 @@ fs.readdirSync('../assets/fonts').forEach(function(name){
 // })
 
 // css += "-------------HTML-------------\n"
-fs.readdirSync('../assets/fonts').forEach(function(name){
+fs.readdirSync('../fonts').forEach(function(name){
 	// css += "<p style='font-family:"+name+"'>"+name+"</p>\n"
 })
 
