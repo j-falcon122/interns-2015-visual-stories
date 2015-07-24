0bd26cb29fa8b2f56af0da2e95fc7199de053510
Sam Kortchmar <samuel.kortchmar@nytimes.com>
2015-07-24 12:12:15 -0400

    entry modal

--- js/controllers/ArticleCtrl.js
+++ js/controllers/ArticleCtrl.js
@@ -1,9 +1,22 @@
-angular.module('Article', ['AssetService', 'ui.bootstrap']).controller("ArticleCtrl", function ($rootScope, $scope, assets) {
+angular.module('Article', ['AssetService', 'ui.bootstrap']).controller("ArticleCtrl", function ($rootScope, $scope, $modal, assets) {
 
     $scope.url = '';
 
     $scope.getArticle = function() {
+        $scope.modalInstance.close();
+        console.log($scope.url);
         $rootScope.$broadcast('article:load', $scope.url);
     }
 
+    var template = '<form ng-submit="$parent.getArticle()" id="search" class="navbar-right input-group input-group-lg row-fluid"><input ng-model="$parent.url" type="text" class="form-control karnak" placeholder="Enter article URL here"><button type="submit" class="input-group-addon btn btn-lg"><span class="glyphicon glyphicon-search"></span></button></form>'
+
+
+    $scope.open = function() {
+        $scope.modalInstance = $modal.open({
+            animation: true,
+            template: template,
+            controller: 'ArticleCtrl',
+            scope: $scope, //the parent of the modal scope.
+        });
+    };
 });
